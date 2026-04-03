54a988172820074737d4ab7e7f457113d34972d2
beslan066 <beslanshamaev1@gmail.com>
2026-04-03 15:01:06 +0300

    убрал запрос токена с необязательных роутов

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-// Маршруты авторизации (без middleware auth:sanctum)
+// Публичные маршруты (без авторизации)
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
     ->middleware('guest')
     ->name('login');
@@ -13,30 +13,34 @@ Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->middleware('auth:sanctum')
     ->name('logout');
 
-// Получение текущего пользователя
+// Публичные маршруты для справочников (ДО регистрации)
+Route::group(['namespace' => 'Api'], function () {
+    Route::group(['namespace' => 'Country', 'prefix' => 'countries'], function () {
+        Route::get('/', [App\Http\Controllers\Api\CountryController::class, 'index']);
+    });
+
+    Route::group(['namespace' => 'District', 'prefix' => 'districts'], function () {
+        Route::get('/', [App\Http\Controllers\Api\DistrictController::class, 'index']);
+    });
+
+    Route::group(['namespace' => 'City', 'prefix' => 'cities'], function () {
+        Route::get('/', [App\Http\Controllers\Api\CityController::class, 'index']);
+    });
+
+    Route::group(['namespace' => 'School', 'prefix' => 'schools'], function () {
+        Route::get('/', [App\Http\Controllers\Api\SchoolController::class, 'index']);
+    });
+});
+
+// Получение текущего пользователя (требует авторизацию)
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Ваши защищенные маршруты
+// Защищенные маршруты (только для авторизованных)
 Route::group(['middleware' => 'auth:sanctum','namespace' => 'Api'], function () {
     Route::group(['namespace' => 'Role', 'prefix' => 'roles'], function () {
-        Route::get('/', [App\Http\Controllers\Api\RoleController::class, 'index'])->name('api.roles.index');
+        Route::get('/', [App\Http\Controllers\Api\RoleController::class, 'index']);
     });
-
-    Route::group(['namespace' => 'Country', 'prefix' => 'countries'], function () {
-        Route::get('/', [App\Http\Controllers\Api\CountryController::class, 'index'])->name('api.countries.index');
-    });
-
-    Route::group(['namespace' => 'District', 'prefix' => 'districts'], function () {
-        Route::get('/', [App\Http\Controllers\Api\DistrictController::class, 'index'])->name('api.districts.index');
-    });
-
-    Route::group(['namespace' => 'City', 'prefix' => 'cities'], function () {
-        Route::get('/', [App\Http\Controllers\Api\CityController::class, 'index'])->name('api.cities.index');
-    });
-
-    Route::group(['namespace' => 'School', 'prefix' => 'schools'], function () {
-        Route::get('/', [App\Http\Controllers\Api\SchoolController::class, 'index'])->name('api.schools.index');
-    });
+    
 });
